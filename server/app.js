50e9cfeb83d1959331bc8575f1825c489a40dc37
MJLord <69679203+MJLord@users.noreply.github.com>
2022-11-10 13:13:32 +0530

    CORS added

--- server/app.js
+++ server/app.js
@@ -2,9 +2,13 @@ import express from "express"
 import logger from "morgan"
 import connectMongo from "./connector/connectMongo"
 import indexRouter from "./routes"
+import cors from "cors"
+
 var app = express()
 
 connectMongo()
+
+app.use(cors())
 
 app.use(logger('dev'))
 
@@ -12,8 +16,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/",indexRouter)
+app.use("/", indexRouter)
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("listening from port 3000")
 })
